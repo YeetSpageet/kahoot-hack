acf3d2c25d7a7018dce45a3bfd99bb5331e0a341
YeetSpageet <51180151+YeetSpageet@users.noreply.github.com>
2019-05-30 09:39:33 +1000

    Update main.go

--- kahoot-crash/main.go
+++ kahoot-crash/main.go
@@ -57,7 +57,7 @@ func main() {
 	msg := kahoot.Message{
 		"data": kahoot.Message{
 			"type":    "message",
-			"gameid":  gamePin,
+			"gameid":  ,
 			"host":    "kahoot.it",
 			"content": string(encodedContent),
 			"id":      6,
